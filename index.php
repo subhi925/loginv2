5f13971464e69024bfe2533f6a899aed7f0c6a56
subhi <subhihamed88@gmail.com>
2025-11-21 18:44:47 +0200

    Final

--- index.php
+++ index.php
@@ -1,35 +1,98 @@
 <?php
 //index.php
 require 'vendor/autoload.php';
-use Firebase\JWT\JWT;
-$error = '';
-if(isset($_POST['login'])){
-    $conn = new PDO('mysql:host=localhost;dbname=testdb', 'root', '');
 
+use Firebase\JWT\JWT;
+
+$error = '';
+if (isset($_POST['login'])) {
+    $conn = new PDO('mysql:host=localhost;dbname=testdb', 'root', '');
+    if (empty($_POST["email"])) {
+        $error = 'Please Enter Email Details';
+    } else if (empty($_POST["password"])) {
+        $error = 'Please Enter Password Details';
+    } else {
+        $query = "SELECT * FROM users WHERE user_email = ?";
+        $statement = $conn->prepare($query);
+        $statement->execute([$_POST["email"]]);
+        $data = $statement->fetch(PDO::FETCH_ASSOC);
+        if ($data) {
+            if ($data['user_password'] ===  $_POST['password']) {
+                $key = '1a3LM3W966D6QTJ5BJb9opunkUcw_d09NCOIJb9QZTsrneqOICoMoeYUDcd_NfaQyR787PAH98Vhue5g938jdkiyIZyJICytKlbjNBtebaHljIR6-zf3A2h3uy6pCtUFl1UhXWnV6madujY4_3SyUViRwBUOP-UudUL4wnJnKYUGDKsiZePPzBGrF4_gxJMRwF9lIWyUCHSh-PRGfvT7s1mu4-5ByYlFvGDQraP4ZiG5bC1TAKO_CnPyd1hrpdzBzNW4SfjqGKmz7IvLAHmRD-2AMQHpTU-hN2vwoA-iQxwQhfnqjM0nnwtZ0urE6HjKl6GWQW-KLnhtfw5n_84IRQ';
+                $token = JWT::encode(
+                    array(
+                        'iat'        =>    time(),
+                        'nbf'        =>    time(),
+                        'exp'        =>    time() + 3600,
+                        'data'    => array(
+                            'user_id'    =>    $data['user_id'],
+                            'user_name'    =>    $data['user_name']
+                        )
+                    ),
+                    $key,
+                    'HS256'
+                );
+                setcookie("token", $token, time() + 3600, "/", "", true, true);
+                header('location:welcom.php');
+            } else {
+                $error = 'Wrong Password';
+            }
+        } else {
+            $error = 'Wrong Email Address';
+        }
+    }
 }
 
-
-
 ?>
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>How to Create Login using JWT Token in PHP</title>
 </head>
+
 <body>
-    <form method="post">
-        <label >Email</label>
-        <input type="email" name="email"/>
-        <label>Password</label>
-        <input type="password" name="password"/>
-        <input type="submit" name="login" value="Login"/>
+    <div class="container">
+        <h1 class="text-center mt-5 mb-5">How to Create Login using JWT Token in PHP</h1>
+        <div class="row">
+            <div class="col-md-4">&nbsp;</div>
+            <div class="col-md-4">
+                <?php
 
-    </form>
-    
+                if ($error !== '') {
+                    echo '<div class="alert alert-danger">' . $error . '</div>';
+                }
+
+                ?>
+                <div class="card">
+                    <div class="card-header">Login</div>
+                    <div class="card-body">
+                        <form method="post">
+                            <div class="mb-3">
+                                <label>Email</label>
+                                <input type="email" name="email" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <label>Password</label>
+                                <input type="password" name="password" class="form-control" />
+                            </div>
+                            <div class="text-center">
+                                <input type="submit" name="login" class="btn btn-primary" value="Login" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
-
-
-
---
+++ logout.php
@@ -0,0 +1,7 @@
+<?php
+
+//logout.php
+
+setcookie("token", "", time() - 3600,  "/", "", true, true);
+
+header('location:index.php');
